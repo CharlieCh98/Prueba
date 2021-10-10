a367720562c9c72b9fb255375f830bbd714bf327
CharlieCh98 <51213051+CharlieCh98@users.noreply.github.com>
2021-10-10 09:23:34 -0500

    Add files via upload

---
+++ Reto/Reto/Reto/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿
+namespace Reto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnIniciar = new System.Windows.Forms.PictureBox();
+            this.btnDetener = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.labelCanela = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.labelVainilla = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.labelChocolate = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnIniciar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnDetener)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 592);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.RoyalBlue;
+            this.panel3.Controls.Add(this.btnIniciar);
+            this.panel3.Controls.Add(this.btnDetener);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Location = new System.Drawing.Point(232, 374);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(328, 197);
+            this.panel3.TabIndex = 1;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIniciar.Image = global::Reto.Properties.Resources.pngegg;
+            this.btnIniciar.Location = new System.Drawing.Point(20, 52);
+            this.btnIniciar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(120, 123);
+            this.btnIniciar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnIniciar.TabIndex = 3;
+            this.btnIniciar.TabStop = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDetener.Image = global::Reto.Properties.Resources.pngegg__1_;
+            this.btnDetener.Location = new System.Drawing.Point(188, 52);
+            this.btnDetener.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(120, 123);
+            this.btnDetener.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnDetener.TabIndex = 4;
+            this.btnDetener.TabStop = false;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(204, 22);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DETENER";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(36, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "INICIAR";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Azure;
+            this.panel2.Controls.Add(this.labelCanela);
+            this.panel2.Controls.Add(this.label11);
+            this.panel2.Controls.Add(this.labelVainilla);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.labelChocolate);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(784, 340);
+            this.panel2.TabIndex = 0;
+            // 
+            // labelCanela
+            // 
+            this.labelCanela.AutoSize = true;
+            this.labelCanela.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCanela.Location = new System.Drawing.Point(705, 295);
+            this.labelCanela.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelCanela.Name = "labelCanela";
+            this.labelCanela.Size = new System.Drawing.Size(21, 23);
+            this.labelCanela.TabIndex = 13;
+            this.labelCanela.Text = "0";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(552, 269);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(153, 46);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "CANTIDAD DE\r\nPRODUCCIÓN:";
+            // 
+            // labelVainilla
+            // 
+            this.labelVainilla.AutoSize = true;
+            this.labelVainilla.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelVainilla.Location = new System.Drawing.Point(426, 298);
+            this.labelVainilla.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelVainilla.Name = "labelVainilla";
+            this.labelVainilla.Size = new System.Drawing.Size(21, 23);
+            this.labelVainilla.TabIndex = 11;
+            this.labelVainilla.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(274, 272);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(153, 46);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "CANTIDAD DE\r\nPRODUCCIÓN:";
+            // 
+            // labelChocolate
+            // 
+            this.labelChocolate.AutoSize = true;
+            this.labelChocolate.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelChocolate.Location = new System.Drawing.Point(166, 298);
+            this.labelChocolate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelChocolate.Name = "labelChocolate";
+            this.labelChocolate.Size = new System.Drawing.Size(21, 23);
+            this.labelChocolate.TabIndex = 9;
+            this.labelChocolate.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(18, 272);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(153, 46);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "CANTIDAD DE\r\nPRODUCCIÓN:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(616, 22);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 23);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "CANELA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(334, 22);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "VAINILLA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(57, 22);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "CHOCOLATE";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Reto.Properties.Resources.donacanela;
+            this.pictureBox3.Location = new System.Drawing.Point(556, 52);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(210, 215);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Reto.Properties.Resources.dona_vainilla;
+            this.pictureBox2.Location = new System.Drawing.Point(279, 52);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(210, 215);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Reto.Properties.Resources.dona_chocolate;
+            this.pictureBox1.Location = new System.Drawing.Point(18, 52);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(210, 215);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 592);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "MAQUINA DE DONAS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnIniciar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnDetener)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox btnIniciar;
+        private System.Windows.Forms.PictureBox btnDetener;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelCanela;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label labelVainilla;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label labelChocolate;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Reto/Reto/Reto/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reto
+{
+    public partial class Form1 : Form
+    {
+        Thread thread;
+        delegate void dlgt(int value);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            thread = new Thread(new ThreadStart(producirDonaChocolate));
+            thread.Start();
+
+            thread = new Thread(new ThreadStart(producirDonaVainilla));
+            thread.Start();
+
+            thread = new Thread(new ThreadStart(producirDonaCanela));
+            thread.Start();
+        }
+
+        //Metodos
+        public void producirDonaChocolate()
+        {
+            for(int i = 0; i <101; i++)
+            {
+                dlgt d1 = new dlgt(contadorDonaChocolate);
+                Invoke(d1, new object[] { i });
+                Thread.Sleep(5000);
+            }
+        }
+        public void producirDonaVainilla()
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                dlgt d2 = new dlgt(contadorDonaVainilla);
+                Invoke(d2, new object[] { i });
+                Thread.Sleep(3000);
+            }
+        }
+        public void producirDonaCanela()
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                dlgt d3 = new dlgt(contadorDonaCanela);
+                Invoke(d3, new object[] { i });
+                Thread.Sleep(10000);
+            }
+        }
+
+        public void contadorDonaChocolate(int value)
+        {
+            labelChocolate.Text = value.ToString();
+        }
+        public void contadorDonaVainilla(int value)
+        {
+            labelVainilla.Text = value.ToString();
+        }
+        public void contadorDonaCanela(int value)
+        {
+            labelCanela.Text = value.ToString();
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < 101; i++)
+                {
+                    dlgt d1 = new dlgt(contadorDonaChocolate);
+                    Invoke(d1, new object[] { i });
+                    Thread.Sleep(5000);
+                }
+
+                for (int i = 0; i < 101; i++)
+                {
+                    dlgt d2 = new dlgt(contadorDonaVainilla);
+                    Invoke(d2, new object[] { i });
+                    Thread.Sleep(3000);
+                }
+          
+                for (int i = 0; i < 101; i++)
+                {
+                    dlgt d3 = new dlgt(contadorDonaCanela);
+                    Invoke(d3, new object[] { i });
+                    Thread.Sleep(10000);
+                }
+            }
+            catch (ThreadAbortException)
+            {
+
+                Thread.ResetAbort();
+            }
+            
+        }
+    }
+}
